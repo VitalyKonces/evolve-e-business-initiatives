038a40685d23248f5d154597ec15735e22b6c32c
VitalyKonces <140142399+VitalyKonces@users.noreply.github.com>
2023-11-06 09:33:57 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,38 @@
+
+        }
+
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+}
+public class RandomDataGenerator {
+    }
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+    public static void main(String[] args) {
+        int[] data = new int[10];
+public class RandomDataGenerator {
+        }
+}
+    public static void main(String[] args) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+    }
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Random Number: " + item);
+
+    for item in data:
+import random
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    main()
+
+if __name__ == "__main__":
+def main():
+
+    data = generate_random_data()
+def generate_random_data():
